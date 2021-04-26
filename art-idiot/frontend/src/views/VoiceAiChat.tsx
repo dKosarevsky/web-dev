58c01e19d8188adb5a57fc02b6c754d550793d31
if <if>
2021-04-26 10:40:18 +0300

    feat: upd voice chat

--- art-idiot/frontend/src/views/VoiceAiChat.tsx
+++ art-idiot/frontend/src/views/VoiceAiChat.tsx
@@ -1,5 +1,15 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 
 export const VoiceAiChat: FC = () => {
-  return <h1>Голосовой чат с Artificial idiot на базе ruGPT-3</h1>;
+  return (
+    <>
+      <h1 className="art-idiot-h1">
+        Voice AI chat
+      </h1>
+
+      <h2>Голосовой чат с Artificial idiot</h2>
+
+      <h2>... в разработке ...</h2>
+    </>
+  );
 };
