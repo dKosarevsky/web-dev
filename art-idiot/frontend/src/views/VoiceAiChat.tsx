08a201253d7b59a1e6009f0939c309bab712cdb8
if <if>
2021-04-30 17:23:28 +0300

    feat: Voice

--- art-idiot/frontend/src/views/VoiceAiChat.tsx
+++ art-idiot/frontend/src/views/VoiceAiChat.tsx
@@ -1,6 +1,20 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
+import {Button} from "antd";
+import {getMessage} from "../utils";
 
 export const VoiceAiChat: FC = () => {
+  const [message, setMessage] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+  const queryBackend = async () => {
+    try {
+      const message = await getMessage();
+      setMessage(message);
+    } catch (err) {
+      setError(err);
+    }
+  };
+
   return (
     <>
       <h1 className="art-idiot-h1">
@@ -10,6 +24,21 @@ export const VoiceAiChat: FC = () => {
       <h2>Голосовой чат с Artificial idiot</h2>
 
       <h2>... в разработке ...</h2>
+
+      <br/>
+      <Button
+        className="pushable"
+        style={{
+          margin: "18px 0 40px 0"
+        }}
+        onClick={() => queryBackend()}
+      >
+        <span className="shadow"> </span>
+        <span className="edge"> </span>
+        <span className="front">
+          Нажми Меня
+        </span>
+      </Button>
     </>
   );
 };
